6e84306caf86a2bfdfb2530f5c9c4e5bee81d118
Moneky <237399958+MonekyTheShep@users.noreply.github.com>
2026-02-01 08:15:20 +0000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -59,7 +59,7 @@ void *moveSnake(void *arg)
             tdata->list->head->snake_node.x += (int) *tdata->moveInterval;
             break;
     }
-    
+
 
     // screen wrapping
     if (tdata->list->head->snake_node.y < 0) tdata->list->head->snake_node.y = screenHeight;
@@ -106,13 +106,14 @@ int main(void)
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
+        // prev pos stored after the head position
         Node *temp = snake.head->next;
 
         // move tail to previous positions and check if those tails are touching head
         while (temp != NULL) {
 
             Rectangle tail = { (float) temp->snake_node.x, (float) temp->snake_node.y,  moveInterval,  moveInterval };
-            DrawRectangleRec(tail, BLUE);
+            DrawRectangleRec(tail, GREEN);
 
             int tailOverlapHeadX = temp->snake_node.x == snake.head->snake_node.x;
             int tailOverlapHeadY = temp->snake_node.y == snake.head->snake_node.y;
@@ -126,6 +127,7 @@ int main(void)
             temp = temp->next;
         }
 
+        // store prev position then move the snake position x and y
         if (!threadRunning && !gameOver)
         {
             threadRunning = true;
@@ -136,11 +138,13 @@ int main(void)
             pthread_create(&moveSnakeThread, NULL, moveSnake, &data);
         }
 
+        // input checking
         if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D) && direction != LEFT) direction = RIGHT;
         if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A) && direction != RIGHT) direction = LEFT;
         if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S) && direction != UP) direction = DOWN;
         if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W) && direction != DOWN) direction = UP;
 
+        // if the apple is overlapping head. I need to add check so it doesn't spawn in the body.
         int appleOverlapSnakeX = (float) snake.head->snake_node.x == apple.x;
         int appleOverlapSnakeY = (float) snake.head->snake_node.y == apple.y;
 
@@ -161,9 +165,9 @@ int main(void)
 
         DrawRectangleRec(apple, RED);
         // Draw the snake head as blue square.
-        DrawRectangleRec(snakeHead, BLUE);
+        DrawRectangleRec(snakeHead, DARKGREEN);
 
-        DrawText(TextFormat("Score: %0i", score), 0, 0, 50, GREEN);
+        DrawText(TextFormat("Score: %0i", score), 0, 0, 50, BLACK);
 
         EndDrawing();
     }
