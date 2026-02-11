dccb1dc008f5d2afd50ad085df331132edfbbc30
Moneky <237399958+MonekyTheShep@users.noreply.github.com>
2026-02-11 06:02:32 +0000

    draw grid

--- src/states/gamestate.c
+++ src/states/gamestate.c
@@ -199,8 +199,20 @@ void updateGameMenu(GameInfo *gameInfo, MenuStates *menuState)
     }
 }
 
+static void drawGrid(void) {
+    for (int i = 0; i < GetScreenWidth() / MOVE_INTERVAL; i++) {
+        DrawLine(i * MOVE_INTERVAL,0,i * MOVE_INTERVAL,GetScreenHeight(), Fade(BLACK, 0.5f));
+    }
+
+    for (int i = 0; i < GetScreenHeight() / MOVE_INTERVAL; i++) {
+        DrawLine(0,i * MOVE_INTERVAL,GetScreenWidth(),i * MOVE_INTERVAL, Fade(BLACK, 0.5f));
+    }
+}
+
 void drawGame(GameInfo *gameInfo, MenuStates *menuState) {
+    drawGrid();
     drawSnake();
+
     DrawText(TextFormat("Score: %0i", score), 0, 0, 50, BLACK);
 
     if (gameOver)
