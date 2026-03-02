1a0a4bb2a30bfda4558edf456d9c335ea99a0dd9
Moneky <237399958+MonekyTheShep@users.noreply.github.com>
2026-03-02 13:52:04 +0000

    chore (constants) comments and organise

--- include/constants.h
+++ include/constants.h
@@ -1,18 +1,17 @@
 #pragma once
 
-#define MOVE_INTERVAL (50)
-
+// Music/Sounds
 #define NUM_OF_MUSICS (2)
 #define NUM_OF_SOUNDS (2)
 
-// Sounds
 #define COLLECT_SOUND (0)
 #define EXPLOSION_SOUND (1)
 
-// Music
 #define MAIN_MENU_MUSIC (0)
 #define GAME_MENU_MUSIC (1)
 
 // Snake
+#define MOVE_INTERVAL (50)
+
 #define SNAKE_TIME_INTERVAL (0.1f)
 
