dc0c356442ebf98b5ea9952777baa681a1010042
Moneky <237399958+MonekyTheShep@users.noreply.github.com>
2026-03-02 13:36:31 +0000

    refactor (main)

--- src/main.c
+++ src/main.c
@@ -19,7 +19,70 @@
 // default menu
 States menuState = TITLE_STATE;
 GameInfo gameInfo = {.musicPlaying = 0};
+Music *currentMusic;
 
+static void changeState(States changeState);
+static void updateDrawFrame(void);
+
+int main(void)
+{
+    // Initialise Raylib
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE);
+    SetTargetFPS(60);
+
+    InitAudioDevice();
+    initializeGameState();
+
+    // load sounds
+    sounds[COLLECT_SOUND] = LoadSound(ASSETS_PATH "/sounds/collect_sound.ogg");
+    sounds[EXPLOSION_SOUND] =  LoadSound(ASSETS_PATH "/sounds/explosion_sound.ogg");
+
+    // load musics
+    musics[MAIN_MENU_MUSIC] = LoadMusicStream(ASSETS_PATH "/music/main_menu.ogg");
+    musics[GAME_MENU_MUSIC] = LoadMusicStream(ASSETS_PATH "/music/game_music.ogg");
+
+
+    currentMusic = &musics[MAIN_MENU_MUSIC];
+    PlayMusicStream(*currentMusic);
+    gameInfo.musicPlaying = 1;
+
+    while (!WindowShouldClose())
+    {
+        if (menuState == EXIT)
+        {
+            break;
+        }
+
+        // music management
+        if (!gameInfo.musicPlaying)
+        {
+            switch (menuState)
+            {
+                case TITLE_STATE:
+                    changeMusic(&currentMusic, MAIN_MENU_MUSIC, musics, &gameInfo);
+                    break;
+                case GAME_STATE:
+                    changeMusic(&currentMusic, GAME_MENU_MUSIC, musics, &gameInfo);
+                    break;
+
+                default: break;
+            }
+        }
+
+        updateDrawFrame();
+    }
+
+    const int numOfSound = sizeof(sounds) / sizeof(sounds[0]);
+    CleanUpSound(sounds, numOfSound);
+
+    const int numOfMusic = sizeof(musics) / sizeof(musics[0]);
+    CleanUpMusic(musics, numOfMusic);
+
+    CloseAudioDevice();
+    CloseWindow();
+
+    return 0;
+}
 
 static void changeState(States changeState)
 {
@@ -50,93 +113,38 @@ static void changeState(States changeState)
     menuState = changeState;
 }
 
-int main(void)
-{
-    // Initialise Raylib
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE);
-    SetTargetFPS(60);
+static void updateDrawFrame(void) {
+    const float deltaTime = GetFrameTime();
+    UpdateMusicStream(*currentMusic);
 
-    InitAudioDevice();
-    initializeGameState();
-
-    // load sounds
-    sounds[COLLECT_SOUND] = LoadSound(ASSETS_PATH "/sounds/collect_sound.ogg");
-    sounds[EXPLOSION_SOUND] =  LoadSound(ASSETS_PATH "/sounds/explosion_sound.ogg");
-
-    // load musics
-    musics[MAIN_MENU_MUSIC] = LoadMusicStream(ASSETS_PATH "/music/main_menu.ogg");
-    musics[GAME_MENU_MUSIC] = LoadMusicStream(ASSETS_PATH "/music/game_music.ogg");
-
-    Music *currentMusic = &musics[MAIN_MENU_MUSIC];
-    PlayMusicStream(*currentMusic);
-    gameInfo.musicPlaying = 1;
-
-    while (!WindowShouldClose())
+    // Update logic for each state
+    switch (menuState)
     {
-        if (menuState == EXIT)
-        {
+        case TITLE_STATE:
+            updateTitleState();
+            if (finishTitleState()) changeState(GAME_STATE);
+            if (exitState()) changeState(EXIT);
             break;
-        }
-
-        const float deltaTime = GetFrameTime();
-        UpdateMusicStream(*currentMusic);
-
-        // music management
-        if (!gameInfo.musicPlaying)
-        {
-            switch (menuState)
-            {
-                case TITLE_STATE:
-                    changeMusic(&currentMusic, MAIN_MENU_MUSIC, musics, &gameInfo);
-                    break;
-                case GAME_STATE:
-                    changeMusic(&currentMusic, GAME_MENU_MUSIC, musics, &gameInfo);
-                    break;
-
-                default: break;
-            }
-        }
-
-        // Update logic for each state
-        switch (menuState)
-        {
-            case TITLE_STATE:
-                updateTitleState();
-                if (finishTitleState()) changeState(GAME_STATE);
-                if (exitState()) changeState(EXIT);
-                break;
-            case GAME_STATE:
-                updateGameState(deltaTime);
-                if (finishGameState()) changeState(TITLE_STATE);
-                break;
-            default:
-                break;
-        }
-
-        // Draw for each state
-        BeginDrawing();
-            ClearBackground(WHITE);
-            switch (menuState)
-            {
-                case TITLE_STATE:
-                    drawTitleState();
-                    break;
-                case GAME_STATE:
-                    drawGameState();
-                    break;
-                default: break;
-            }
-        EndDrawing();
+        case GAME_STATE:
+            updateGameState(deltaTime);
+            if (finishGameState()) changeState(TITLE_STATE);
+            break;
+        default:
+            break;
     }
 
-    const int numOfSound = sizeof(sounds) / sizeof(sounds[0]);
-    CleanUpSound(sounds, numOfSound);
-
-    const int numOfMusic = sizeof(musics) / sizeof(musics[0]);
-    CleanUpMusic(musics, numOfMusic);
-
-    CloseAudioDevice();
-    CloseWindow();
-
-    return 0;
+    // Draw for each state
+    BeginDrawing();
+    ClearBackground(WHITE);
+    switch (menuState)
+    {
+        case TITLE_STATE:
+            drawTitleState();
+            break;
+        case GAME_STATE:
+            drawGameState();
+            break;
+        default: break;
+    }
+    EndDrawing();
 }
